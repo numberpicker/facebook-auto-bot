ef8253a491b41f1d94b39ef9e4bd9d38961a1067
Games <meraalshah447@gmail.com>
2026-09-24 03:40:01 +0500

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -59,7 +59,7 @@ export const env = {
     return optional("GROQ_API_KEY");
   },
   get geminiApiKey() {
-    return optional("GEMINI_API_KEY");
+    return optional("AQ.Ab8RN6LUmaA-6iv6nOHzaYjyq9XsrDFEtjPd_S8Lv9Kbn4vnIQ");
   },
 
   // Free image sources
